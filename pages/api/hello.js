2e34f4098d5fc5598a2cede458b9c45389cecdde
Rholang <rholang@protonmail.com>
2021-07-04 08:23:15 +0200

    up

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  const result = { name: "John Doe" };
+  res.setHeader(
+    "Cache-Control",
+    result.error
+      ? "public, s-maxage=60, max-age=60, stale-while-revalidate=60"
+      : "public, immutable, s-maxage=31536000, max-age=31536000, stale-while-revalidate=60"
+  );
+  res.statusCode = 200;
+  res.json(result);
+};
